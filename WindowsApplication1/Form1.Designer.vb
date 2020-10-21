f17861a4c6c1823e30bf6e782335375363ad3e01
isaacthoman <isaacthoman@outlook.com>
2020-10-21 15:47:52 -0400

    Another initial commit

---
+++ WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,126 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.leftPaddle = New System.Windows.Forms.PictureBox()
+        Me.rightPaddle = New System.Windows.Forms.PictureBox()
+        Me.ball = New System.Windows.Forms.PictureBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.ScoreLabel = New System.Windows.Forms.Label()
+        Me.debugLabel = New System.Windows.Forms.Label()
+        Me.scaleTimer = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.leftPaddle, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.rightPaddle, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ball, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'leftPaddle
+        '
+        Me.leftPaddle.BackColor = System.Drawing.Color.White
+        Me.leftPaddle.Location = New System.Drawing.Point(40, 140)
+        Me.leftPaddle.Name = "leftPaddle"
+        Me.leftPaddle.Size = New System.Drawing.Size(40, 150)
+        Me.leftPaddle.TabIndex = 0
+        Me.leftPaddle.TabStop = False
+        '
+        'rightPaddle
+        '
+        Me.rightPaddle.BackColor = System.Drawing.Color.White
+        Me.rightPaddle.Location = New System.Drawing.Point(900, 250)
+        Me.rightPaddle.Name = "rightPaddle"
+        Me.rightPaddle.Size = New System.Drawing.Size(40, 150)
+        Me.rightPaddle.TabIndex = 1
+        Me.rightPaddle.TabStop = False
+        '
+        'ball
+        '
+        Me.ball.BackColor = System.Drawing.Color.White
+        Me.ball.Location = New System.Drawing.Point(472, 260)
+        Me.ball.Name = "ball"
+        Me.ball.Size = New System.Drawing.Size(40, 40)
+        Me.ball.TabIndex = 2
+        Me.ball.TabStop = False
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 30
+        '
+        'ScoreLabel
+        '
+        Me.ScoreLabel.AutoSize = True
+        Me.ScoreLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ScoreLabel.ForeColor = System.Drawing.Color.White
+        Me.ScoreLabel.Location = New System.Drawing.Point(453, 20)
+        Me.ScoreLabel.Name = "ScoreLabel"
+        Me.ScoreLabel.Size = New System.Drawing.Size(78, 33)
+        Me.ScoreLabel.TabIndex = 3
+        Me.ScoreLabel.Text = "0 - 0"
+        '
+        'debugLabel
+        '
+        Me.debugLabel.AutoSize = True
+        Me.debugLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.debugLabel.ForeColor = System.Drawing.Color.White
+        Me.debugLabel.Location = New System.Drawing.Point(12, 519)
+        Me.debugLabel.Name = "debugLabel"
+        Me.debugLabel.Size = New System.Drawing.Size(176, 33)
+        Me.debugLabel.TabIndex = 4
+        Me.debugLabel.Text = "debugLabel"
+        Me.debugLabel.UseWaitCursor = True
+        '
+        'scaleTimer
+        '
+        Me.scaleTimer.Enabled = True
+        Me.scaleTimer.Interval = 5000
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(984, 561)
+        Me.Controls.Add(Me.debugLabel)
+        Me.Controls.Add(Me.ScoreLabel)
+        Me.Controls.Add(Me.ball)
+        Me.Controls.Add(Me.rightPaddle)
+        Me.Controls.Add(Me.leftPaddle)
+        Me.Name = "Form1"
+        Me.Text = "Pong!"
+        CType(Me.leftPaddle, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.rightPaddle, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ball, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents leftPaddle As PictureBox
+    Friend WithEvents rightPaddle As PictureBox
+    Friend WithEvents ball As PictureBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents ScoreLabel As Label
+    Friend WithEvents debugLabel As Label
+    Friend WithEvents scaleTimer As Timer
+End Class
---
+++ WindowsApplication1/Form1.vb
@@ -0,0 +1,248 @@
+﻿
+Public Class Form1
+
+
+    Dim LeftScore As Integer
+    Dim RightScore As Integer
+
+    Dim LeftDown As Boolean
+    Dim RightDown As Boolean
+    Dim UpDown As Boolean
+    Dim DownDown As Boolean
+    Dim keyWDown As Boolean
+    Dim keySDown As Boolean
+
+
+    Public leftTouching As Boolean
+    Public rightTouching As Boolean
+
+
+
+    Function movePlayer()
+        If (UpDown = True) Then
+            rightPaddle.Location = New Point(rightPaddle.Location.X, rightPaddle.Location.Y - 5)
+
+        End If
+        If (DownDown = True) Then
+            rightPaddle.Location = New Point(rightPaddle.Location.X, rightPaddle.Location.Y + 5)
+
+        End If
+
+
+        If (keyWDown = True) Then
+            leftPaddle.Location = New Point(leftPaddle.Location.X, leftPaddle.Location.Y - 5)
+
+        End If
+        If (keySDown = True) Then
+            leftPaddle.Location = New Point(leftPaddle.Location.X, leftPaddle.Location.Y + 5)
+
+        End If
+
+    End Function
+
+
+    Function LeftColisionTest()
+        leftTouching = False
+        If (ball.Location.X + ball.Width > leftPaddle.Location.X And ball.Location.X < leftPaddle.Location.X + leftPaddle.Width) Then
+            'Width matches
+            If (ball.Location.Y + ball.Height > leftPaddle.Location.Y And ball.Location.Y < leftPaddle.Location.Y + leftPaddle.Height) Then
+                'height matches
+                leftTouching = True
+
+            End If
+        End If
+
+    End Function
+
+
+
+    Function RightColisionTest()
+        rightTouching = False
+        If (ball.Location.X + ball.Width > rightPaddle.Location.X And ball.Location.X < rightPaddle.Location.X + rightPaddle.Width) Then
+            'Width matches
+            If (ball.Location.Y + ball.Height > rightPaddle.Location.Y And ball.Location.Y < rightPaddle.Location.Y + rightPaddle.Height) Then
+                'height matches
+                rightTouching = True
+
+            End If
+        End If
+
+    End Function
+
+    Public velocityX As Integer
+    Public velocityY As Integer
+    Public leftVelocityY As Integer
+    Public leftVelocityX As Integer
+
+
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        LeftColisionTest()
+        RightColisionTest()
+
+
+        If (rightTouching = True) Then
+            velocityY = Int((4 - 2 + 1) * Rnd() + 2)
+            velocityX = -10
+        End If
+
+        If (leftTouching = True) Then
+            'velocityY = Int((6 - 2 + 1) * Rnd() + 2)
+            If (ball.Location.Y < Me.Height / 2) Then
+                velocityY = 5
+            End If
+            If (ball.Location.Y > Me.Height / 2) Then
+                velocityY = -5
+            End If
+            velocityX = 10
+        End If '  ------------------------ This was the old bot
+
+        ' '       If (ball.Location.Y < leftPaddle.Location.Y) Then
+        '        leftVelocityY = -3
+        '
+        '        End If
+        '        If (ball.Location.Y > leftPaddle.Location.Y) Then
+        '        leftVelocityY = 3
+
+        '        End If
+
+
+
+
+
+        '                ---------------------------------------------------------------put scoring here
+        If (ball.Location.X > Me.Width - ball.Width) Then
+            velocityX = -5
+            LeftScore = LeftScore + 1
+            ScoreLabel.Text = (LeftScore & " - " & RightScore)
+            ball.Location = New Point(472, 260)
+
+        End If
+        If (ball.Location.X < 0) Then
+            velocityX = 5
+            RightScore = RightScore + 1
+            ball.Location = New Point(472, 260)
+            ScoreLabel.Text = (LeftScore & " - " & RightScore)
+        End If
+
+        If (ball.Location.Y > Me.Height - (ball.Height * 2)) Then
+
+            velocityY = -5
+        End If
+
+        If (ball.Location.Y < 0) Then
+
+            velocityY = 5
+        End If
+
+        movePlayer()
+
+        leftPaddle.Location = New Point(leftPaddle.Location.X + leftVelocityX, leftPaddle.Location.Y + leftVelocityY)
+
+
+        ball.Location = New Point(ball.Location.X + velocityX, ball.Location.Y + velocityY)
+    End Sub
+
+    Private Sub PictureBox3_Click(sender As Object, e As EventArgs) Handles ball.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        velocityX = 10
+        debugLabel.Visible = False '   ---------------------------------------------hides the key value thing
+
+    End Sub
+
+
+
+    'Next 65 lines or so detect key presses
+    Private Sub Form1_KeyDown(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles Me.KeyDown
+        If e.Control Or e.Shift Or e.Alt Then
+
+        End If
+
+
+        '     LeftDown = False
+        '     RightDown = False
+        '     UpDown = False
+        '     DownDown = False
+
+        If e.KeyValue = 37 Then
+            LeftDown = True
+            RightDown = False
+            UpDown = False
+            DownDown = False
+        End If
+        If e.KeyValue = 38 Then
+            LeftDown = False
+            RightDown = False
+            UpDown = True
+            DownDown = False
+        End If
+        If e.KeyValue = 39 Then
+            LeftDown = False
+            RightDown = True
+            UpDown = False
+            DownDown = False
+        End If
+        If e.KeyValue = 40 Then
+            LeftDown = False
+            RightDown = False
+            UpDown = False
+            DownDown = True
+        End If
+
+        If e.KeyValue = 87 Then
+            keyWDown = True
+            keySDown = False
+        End If
+
+        If e.KeyValue = 83 Then
+            keyWDown = False
+            keySDown = True
+        End If
+
+        '    debugLabel.Text = e.KeyValue
+    End Sub
+
+    Private Sub Form1_KeyuP(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles Me.KeyUp
+        If e.Control Or e.Shift Or e.Alt Then
+
+        End If
+
+
+
+
+        If e.KeyValue = 37 Then
+            LeftDown = False
+        End If
+        If e.KeyValue = 38 Then
+
+            UpDown = False
+
+        End If
+        If e.KeyValue = 39 Then
+
+            RightDown = False
+
+        End If
+        If e.KeyValue = 40 Then
+            DownDown = False
+        End If
+
+        If e.KeyValue = 87 Then
+            keyWDown = False
+
+        End If
+
+        If e.KeyValue = 83 Then
+
+            keySDown = False
+        End If
+
+    End Sub
+
+    Private Sub scaleTimer_Tick(sender As Object, e As EventArgs) Handles scaleTimer.Tick
+        rightPaddle.Location = New Point((Me.Width - 50 - rightPaddle.Width), rightPaddle.Location.Y)
+    End Sub
+End Class
